67e3abc56e3152327086ffbdbc1d1076b27a6d37
xfnw <xfnw@riseup.net>
2023-04-30 18:15:35 -0400

    simulate multi-hashing bug output

---
+++ ngircchain.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+#include "portab.h"
+#include <assert.h>
+#include <string.h>
+#include "defines.h"
+#include "tool.h"
+#include "hash.c"
+#include <stdio.h>
+
+int a;
+int b;
+int c;
+int d;
+
+int
+main(int argc, char *argv[]) {
+
+	char *salt = argv[1];
+	char out_buffer[255];
+	strcpy(out_buffer,argv[2]);
+	int num = atoi(argv[3]);
+	printf("%04i: %s\n", 0, out_buffer);
+
+	for (int i=1; i <= num; i++) {
+					char host_buffer[255];
+					strcpy(host_buffer,out_buffer);
+					strcat(host_buffer,salt);
+
+					sprintf(out_buffer,"%x",Hash(host_buffer));
+					printf("%04i: %s\n", i, out_buffer);
+	}
+
+}
