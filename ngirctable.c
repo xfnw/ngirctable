7d17d6fb02fde89e1d26e0a2dc2d94d9bab5c179
xfnw <xfnw@riseup.net>
2023-05-01 01:47:50 -0400

    whoops

--- ngirctable.c
+++ ngirctable.c
@@ -20,8 +20,8 @@ main(int argc, char *argv[]) {
 		for (b=1; b<=255; b++) {
 			for (c=1; c<=255; c++) {
 				for (d=1; d<=255; d++) {
-					char *host;
-					asprintf(&host,"%u.%u.%u.%u",a,b,c,d);
+					char host[18];
+					sprintf(host,"%u.%u.%u.%u",a,b,c,d);
 
 					char host_buffer[255];
 					strcpy(host_buffer,host);
