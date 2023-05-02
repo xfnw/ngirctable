ac4d0ae670944afa4dcaca3880cf92c612c01220
xfnw <xfnw@riseup.net>
2023-05-01 21:28:18 -0400

    add offset

--- ngircgen.c
+++ ngircgen.c
@@ -11,9 +11,12 @@ int
 main(int argc, char *argv[]) {
 	char *salt = argv[1];
 	int num = atoi(argv[2]);
+	int off = 0;
+	if (argc > 3)
+		off = atoi(argv[3]);
+	int max = num+off;
 
-
-	for (int i=1; i <= num; i++) {
+	for (int i=off; i < max; i++) {
 		char host_buffer[255];
 		sprintf(host_buffer,"%x",i);
 		strcat(host_buffer,salt);
